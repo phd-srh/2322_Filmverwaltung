0b08f5320d28bdfc55d3593be823e87402c28e09
Dr. Dillinger <peter.dillinger@srh.de>
2023-05-16 14:27:14 +0200

    Interface für den Datenzugriff auf mögliche Film-Datenbanken erstellt.

--- src/dao/FilmDAO.java
+++ src/dao/FilmDAO.java
@@ -1,4 +1,25 @@
 package dao;
 
-public class FilmDAO {
+import model.Film;
+import model.Genre;
+
+import java.util.List;
+
+public interface FilmDAO {
+
+    boolean insertMovie(Film f);
+    Film getMovieByNumber(int fnr);
+    List<Film> getAllMovies();
+    boolean updateMovie(int fnr, Film f);
+    boolean deleteMovie(int fnr);
+    int getHighestMovieNumber();
+
+    boolean insertGenre(Genre g);
+    Genre getGenreByNumber(int gnr);
+    Genre getGenreByName(String name);
+    List<Genre> getAllGenres();
+    boolean updateGenre(int gnr, Genre g);
+    boolean deleteGenre(int gnr);
+    int getHighestGenreNumber();
+
 }
