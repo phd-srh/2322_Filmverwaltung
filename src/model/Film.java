1a30aa4a5379d1343f7580b3bf6a90a8c1a7a366
Dr. Dillinger <peter.dillinger@srh.de>
2023-05-16 14:33:17 +0200

    Model vom Film und Genre ergänzt.

--- src/model/Film.java
+++ src/model/Film.java
@@ -1,4 +1,53 @@
 package model;
 
 public class Film {
+    private int filmnummer;
+    private String titel;
+    private Genre genre;
+    private double rating;
+    private int erscheinungsjahr;
+
+    public Film(int filmnummer, String titel, Genre genre, double rating, int erscheinungsjahr) {
+        this.filmnummer = filmnummer;
+        this.titel = titel;
+        this.genre = genre;
+        this.rating = rating;
+        this.erscheinungsjahr = erscheinungsjahr;
+    }
+
+    public int getFilmnummer() {
+        return filmnummer;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getErscheinungsjahr() {
+        return erscheinungsjahr;
+    }
+
+    public void setErscheinungsjahr(int erscheinungsjahr) {
+        this.erscheinungsjahr = erscheinungsjahr;
+    }
 }
--- src/model/Genre.java
+++ src/model/Genre.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Genre {
+    private int genrenummer;
+    private String bezeichnung;
+
+    public Genre(int genrenummer, String bezeichnung) {
+        this.genrenummer = genrenummer;
+        this.bezeichnung = bezeichnung;
+    }
+
+    public int getGenrenummer() {
+        return genrenummer;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
 }
